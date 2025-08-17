099ff45a2bdafda0388c4543e18feb87e7d4fbae
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2025-08-17 03:18:09 +0000

    Sync LeetCode submission - Next Permutation (java)

---
+++ my-folder/problems/next_permutation/solution.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: find first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: if pivot found
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: reverse the suffix
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
