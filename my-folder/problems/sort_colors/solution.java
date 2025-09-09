4766892ffbf47eb6ad358d75632944562b808e21
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2025-09-09 04:03:56 +0000

    Sync LeetCode submission - Sort Colors (java)

--- my-folder/problems/sort_colors/solution.java
+++ my-folder/problems/sort_colors/solution.java
@@ -1,15 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer,Integer>  map=new HashMap();
-        for(int i=0;i<nums.length;i++) 
-        map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        int index=0;
-        for(int i=0;i<3;i++)
-            if(map.containsKey(i))
-                for(int j=0;j<map.get(i);j++)
-                nums[index++]=i;
-        
+     int low=0,mid=0,high=nums.length-1;
+     while(mid<=high){
+        if(nums[mid]==0){
+         swap(nums,low,mid);
+         low++;
+         mid++;
+        }else if(nums[mid]==2){
+            swap(nums,mid,high);
+            high--;
+        }else 
+        mid++;
     
-        
+     } 
+    }
+    public void swap(int[] nums,int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
 }
