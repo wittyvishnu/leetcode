7f6f9599d86e163faaa7501678143fb362b70727
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2025-09-21 04:18:43 +0000

    Sync LeetCode submission - Valid Palindrome II (java)

---
+++ my-folder/problems/valid_palindrome_ii/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
