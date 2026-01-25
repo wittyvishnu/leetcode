42ecfca835910a13db848753e55c47e8876f18b5
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-01-25 06:09:31 +0000

    Sync LeetCode submission - Minimum Add to Make Parentheses Valid (java)

---
+++ my-folder/problems/minimum_add_to_make_parentheses_valid/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int op=0,cl=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='(')op++;
+            else if(ch==')' && op>0) op--;
+            else cl++;
+            
+        }
+        return op+cl;
+    }
+}
