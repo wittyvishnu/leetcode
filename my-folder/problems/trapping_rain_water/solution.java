bd5c56755f19dd1ac29e775ceaf2c94fd94bb5a3
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-01-25 05:48:46 +0000

    Sync LeetCode submission - Trapping Rain Water (java)

--- my-folder/problems/trapping_rain_water/solution.java
+++ my-folder/problems/trapping_rain_water/solution.java
@@ -1,32 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int[] left=maxToLeft(height);
-        int[] right=maxToRight(height);
-        int rainwater=0;
-        for(int i=0;i<height.length;i++){
-         int temp=Math.min(left[i],right[i])-height[i];
-         if(temp>0)rainwater+=temp;
+        int n=height.length;
+        int[] suffixMax=new int[n];
+        int temp=0;
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            temp=(temp<height[i])?height[i]:temp;
+            suffixMax[i]=temp;
         }
-        return rainwater;
+        temp=0;
+        for(int i=0;i<n;i++){
+            temp=(temp<height[i])?height[i]:temp;
+            int water= Math.min(temp,suffixMax[i])-height[i];
+            res+=(water>0)?water:0;
+        }
+        return res;
 
-        
-    }
-    public int[] maxToLeft(int[] height){
-        int max=0;
-        int[] left=new int[height.length];
-        for(int i=0;i<height.length;i++){
-            max=Math.max(height[i],max);
-            left[i]=max;
-        }
-        return left;
-    }
-    public int[] maxToRight(int[] height){
-        int max=0;
-        int[] right=new int[height.length];
-        for(int i=height.length-1;i>=0;i--){
-            max=Math.max(height[i],max);
-            right[i]=max;
-        }
-        return right;
     }
 }
