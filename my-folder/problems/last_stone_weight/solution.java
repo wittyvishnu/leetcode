b2ab8ff2414c004dae687f11025dde4788d9d1f8
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-02-01 12:11:52 +0000

    Sync LeetCode submission - Last Stone Weight (java)

---
+++ my-folder/problems/last_stone_weight/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        for(int stone:stones)
+        pq.offer(stone);
+        while(pq.size()>1){
+            int weight=pq.poll()-pq.poll();
+            if (weight!=0) pq.offer(weight);
+        }
+         return pq.isEmpty() ? 0 : pq.peek();
+    }
+}
