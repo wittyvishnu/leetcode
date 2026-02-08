14feb0794250d3e9c66edc7f782f0c46ea62d008
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-02-08 13:28:03 +0000

    Sync LeetCode submission - Jump Game II (java)

--- my-folder/problems/jump_game_ii/solution.java
+++ my-folder/problems/jump_game_ii/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-       int jumps=0;
-       int n=nums.length-1;
-       int i=0;
-       while(i<n){
-        if(nums[i]>=n-i){
-            jumps++;
-            return jumps;
-        }
-        else{
-            int temp=i+1;
-            for (int j = i + 2; j <= i + nums[i] && j < nums.length; j++) {
-                if (j + nums[j] >= temp + nums[temp]) {
-                    temp = j;
-                }
-            }
-            i=temp;
-            jumps++;
-        }
-       }
-       return jumps;
+        int n=nums.length;
+        if(n==1)return 0;
+        int destination=n-1;
+        int coverage=0;
+        int lastJumpedIdx=0;
+        int jumps=0;
+        for(int i=0;i<n;i++){
+            coverage=Math.max(coverage,i+nums[i]);
+            if(i==lastJumpedIdx){
+                lastJumpedIdx=coverage;
+                jumps++;
+                if(coverage>=destination)
+                return jumps;
 
+            }
+
+        }
+        return jumps;
     }
 }
