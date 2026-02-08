b7b4f1eb2d976a76c2347b4c47ce4aa2b38b229c
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-02-08 12:08:26 +0000

    Sync LeetCode submission - Jump Game (java)

--- my-folder/problems/jump_game/solution.java
+++ my-folder/problems/jump_game/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n=nums.length-1;
-        int maxindex=0;
-        for(int i=0;i<=n;i++){
-            if(maxindex>=n)return true;
-            if(i>maxindex)return false;
-            maxindex=(i+nums[i]>maxindex)?i+nums[i]:maxindex;
-        
+        int n=nums.length;
+        int goal=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]+i>=goal)
+            goal=i;
         }
-        return true;
+        return goal==0;
     }
 }
