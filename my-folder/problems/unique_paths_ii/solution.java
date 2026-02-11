1a80f29248710e71b8c5bcfedfdd959da5aaeb4c
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-02-11 01:52:08 +0000

    Sync LeetCode submission - Unique Paths II (java)

--- my-folder/problems/unique_paths_ii/solution.java
+++ my-folder/problems/unique_paths_ii/solution.java
@@ -2,19 +2,28 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        for(int i=n-1;i>=0;i--)
-        if(obstacleGrid[m-1][i]==1)obstacleGrid[m-1][i]=0;
+        int[][] dp=new int[m][n];
+        if(obstacleGrid[0][0]==1)return 0;
+        dp[0][0]=1;
+    
+    for (int i = 1; i < m; i++)
+        if(obstacleGrid[i][0]==1)
+        dp[i][0] = 0;
         else
-        obstacleGrid[m-1][i]=(i==n-1)?1:obstacleGrid[m-1][i+1];
-        
-        for(int i=m-2;i>=0;i--)
-        obstacleGrid[i][n-1]=(obstacleGrid[i][n-1]==1)?0:obstacleGrid[i+1][n-1];
-
-        for(int i=m-2;i>=0;i--)
-        for(int j=n-2;j>=0;j--)
-        obstacleGrid[i][j]=(obstacleGrid[i][j]==1)?0:obstacleGrid[i+1][j]+obstacleGrid[i][j+1];
-        
-        return obstacleGrid[0][0];
-        
+        dp[i][0]=dp[i-1][0];
+    for (int j = 1; j < n; j++)
+        if(obstacleGrid[0][j]==1)
+        dp[0][j] = 0;
+        else
+        dp[0][j]=dp[0][j-1];
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(obstacleGrid[i][j]==1)
+            dp[i][j]=0;
+            else
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
     }
 }
