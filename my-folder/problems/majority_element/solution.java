b55276d70a8584f95223c3aa1cb746ca0d70151e
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-02-26 11:09:07 +0000

    Sync LeetCode submission - Majority Element (java)

--- my-folder/problems/majority_element/solution.java
+++ my-folder/problems/majority_element/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++)
-        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        n=n/2;
-        for(Map.Entry<Integer,Integer> entry:map.entrySet())
-        if(entry.getValue()>n)
-        return entry.getKey();
-        return 0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length/2;
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.containsKey(num)&&map.get(num)>n)
+            return num;
+        }
+        return -1;
     }
-    
 }
