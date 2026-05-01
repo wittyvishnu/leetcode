14d55841446688de24c178964ed8e1d49d1600a7
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-05-01 04:38:51 +0000

    Sync LeetCode submission - Daily Temperatures (java)

--- my-folder/problems/daily_temperatures/solution.java
+++ my-folder/problems/daily_temperatures/solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st=new Stack<>();
         int n=temperatures.length;
         int[] ans=new int[n];
-        Stack<Integer> st=new Stack<>();
         for(int i=n-1;i>=0;i--){
-            if(st.isEmpty())st.push(i);
-            else{
-                while(!st.isEmpty()&& temperatures[st.peek()]<=temperatures[i])
-                st.pop();
-                if(!st.isEmpty())ans[i]=st.peek()-i;
-                st.push(i);
-                
-                
-            }
+            while(!st.isEmpty()&&temperatures[st.peek()]<=temperatures[i])
+            st.pop();
+            if(st.isEmpty())ans[i]=0;
+            else ans[i]=st.peek()-i;
+            st.push(i);
         }
         return ans;
     }
