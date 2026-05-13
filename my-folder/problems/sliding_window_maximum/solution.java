4879b3668d7b1ad51c9ba4dca40099ff51e939b0
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-05-13 13:33:48 +0000

    Sync LeetCode submission - Sliding Window Maximum (java)

--- my-folder/problems/sliding_window_maximum/solution.java
+++ my-folder/problems/sliding_window_maximum/solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-         if (nums == null || k == 0) return new int[0];
-        
-        int n = nums.length;
-        int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-
-        for(int i=0;i<n;i++){
-            if(!dq.isEmpty()&&i>k-1&& dq.peekFirst()<i-k+1)
-            dq.pollFirst();
+        ArrayDeque<Integer> dq= new ArrayDeque<>();
+        int[] ans=new int[nums.length-k+1];
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
             while(!dq.isEmpty()&&nums[dq.peekLast()]<=nums[i])
             dq.pollLast();
-            dq.offerLast(i);
-            if(i>=k-1)
-            result[i-k+1]=nums[dq.peekFirst()];
+            dq.addLast(i);
+            if(i>=k-1){
+                while(dq.peekFirst()<i-k+1)
+                dq.pollFirst();
+                ans[idx++]=nums[dq.peekFirst()];
+
             
+            }
 
         }
-        return result;
+        return ans;
+
+
+        
     }
 }
