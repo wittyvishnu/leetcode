fb969e34562ef4b1a8b0e9bbda853fc9f3f2bc9b
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-05-18 05:11:34 +0000

    Sync LeetCode submission - Balanced Binary Tree (java)

--- my-folder/problems/balanced_binary_tree/solution.java
+++ my-folder/problems/balanced_binary_tree/solution.java
@@ -15,14 +15,15 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-       return  depth(root)!=-1;
+        return depth(root)!=-1;
     }
-    private int depth(TreeNode root){
+    public int depth(TreeNode root){
         if(root==null)return 0;
         int l=depth(root.left);
         int r=depth(root.right);
         if(l==-1||r==-1)return -1;
         if(Math.abs(l-r)>1)return -1;
-       return 1 + Math.max(l, r);
+        return Math.max(l,r)+1;
+
     }
 }
