947a6ecc26408056bd00b12d253a7abeebc1183c
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-05-18 05:18:20 +0000

    Sync LeetCode submission - Same Tree (java)

--- my-folder/problems/same_tree/solution.java
+++ my-folder/problems/same_tree/solution.java
@@ -15,9 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        return isSame(p,q);
+    }
+    public boolean isSame(TreeNode p,TreeNode q){
+        if(p==null&&q==null)return true;
+        if((p!=null&&q==null) || (p==null&&q!=null))return false;
+        if(p.val!=q.val)return false;
+        return isSame(p.left,q.left)&&isSame(p.right,q.right);
+
     }
 }
