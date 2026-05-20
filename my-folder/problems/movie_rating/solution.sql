5254df0a3c3f5704091d7a8249ab83c572e0f9de
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-05-20 11:41:57 +0000

    Sync LeetCode submission - Movie Rating (mysql)

---
+++ my-folder/problems/movie_rating/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+(select u.name as results from MovieRating mr
+join Users u on u.user_id=mr.user_id
+group by u.user_id,u.name
+order by count(*) desc,u.name
+limit 1)
+
+union  all
+
+(select m.title as results from MovieRating mr
+join Movies m on m.movie_id=mr.movie_id
+where mr.created_at between '2020-02-01' and '2020-02-29'
+group by m.movie_id,m.title
+order by avg(mr.rating) desc,m.title
+limit 1)
