49d754d2eb8d040075a1526ea15aa1e00674c3ed
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-06-30 13:18:34 +0000

    Sync LeetCode submission - Palindromic Substrings (java)

--- my-folder/problems/palindromic_substrings/solution.java
+++ my-folder/problems/palindromic_substrings/solution.java
@@ -1,28 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
+        char[] ch=s.toCharArray();
+        int n=ch.length;
         int count=0;
-        char[] arr=s.toCharArray();
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            int left=i-1;
-            int right=i+1;
+        for(int i=0;i<n-1;i++){
+            count+=count(ch,i,i);
+            count+=count(ch,i,i+1);
+        }
+        count+=count(ch,n-1,n-1);
+        return count;
+    }
+    public int count(char[] ch,int start,int end){
+        int n=ch.length;
+        int count=0;
+        while(start>=0 && end<n && start<=end && ch[start]==ch[end]){
+            start--;
+            end++;
             count++;
-            while(left>=0&&right<n&&arr[left]==arr[right]){   //odd center
-                count++;
-                left--;
-                right++;
-            }
-            if(i<=n-2&&arr[i]==arr[i+1]){
-                count++;
-                left=i-1;
-                right=i+2;
-                while(left>=0&&right<n&&arr[left]==arr[right]){   //Even center
-                count++;
-                left--;
-                right++;
-            }
-            }
-
         }
         return count;
 
