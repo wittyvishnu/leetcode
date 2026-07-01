c718f72117d65798c0762506c68f18229e296aaa
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-01 05:17:02 +0000

    Sync LeetCode submission - Target Sum (java)

--- my-folder/problems/target_sum/solution.java
+++ my-folder/problems/target_sum/solution.java
@@ -1,19 +1,20 @@
 class Solution {
+    int n;
+    int ways=0;
     public int findTargetSumWays(int[] nums, int target) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        counter.put(0, 1);
-        for (int n : nums) {
-            Map<Integer, Integer> temp = new HashMap<>();
-            for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-                int total = entry.getKey();
-                int count = entry.getValue();
-                temp.put(total + n, temp.getOrDefault(total + n, 0) + count);
-                temp.put(total - n, temp.getOrDefault(total - n, 0) + count);
-            }
-            counter.clear();
-            counter=temp;
+        n=nums.length;
+        checkAll(nums,target,0,0);
+        return ways;
 
+    }
+    public void checkAll(int[] nums,int target,int index,int sum){
+        if(index==n){
+            if(sum==target)
+            ways++;
+            return;
         }
-        return counter.getOrDefault(target, 0);  
+        checkAll(nums,target,index+1,sum+nums[index]);
+        checkAll(nums,target,index+1,sum-nums[index]);
+
     }
 }
