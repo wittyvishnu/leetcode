ccec19a944fd456e32d7505c8ecdea48afaa8b4f
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-01 06:09:30 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (java)

--- my-folder/problems/partition_equal_subset_sum/solution.java
+++ my-folder/problems/partition_equal_subset_sum/solution.java
@@ -5,20 +5,27 @@ class Solution {
         for(int num:nums)
         sum+=num;
         if(sum%2==1)return false;
-        Boolean[][] dp=new Boolean[n][sum+1];
-        return  isSubsetSum(nums,n-1,sum/2,dp);
+        
+        return  isSubsetSum(nums,sum/2);
 
     }
-    public boolean isSubsetSum(int[] nums,int index,int target,Boolean[][] dp){
-        if(target==0)return true;
-        if(index==0)return target==nums[0];
-        if(dp[index][target]!=null)
-        return dp[index][target];
-        boolean notTaken=isSubsetSum(nums,index-1,target,dp);
-        boolean taken=false;
-        if(nums[index]<=target)
-        taken=isSubsetSum(nums,index-1,target-nums[index],dp);
-        dp[index][target]=notTaken||taken;
-        return dp[index][target];
+    public boolean isSubsetSum(int[] nums,int sum){
+        int n=nums.length;
+        boolean[][] dp=new boolean[n][sum+1];
+        for(int i=1;i<sum+1;i++)
+        if(i==nums[0])dp[0][i]=true;
+
+        for(int i=0;i<n;i++)
+        dp[i][0]=true;
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                boolean notTaken=dp[i-1][j];
+                boolean take=(j >= nums[i]) && dp[i-1][j-nums[i]];
+                
+                dp[i][j]= take|| notTaken;
+            }
+        }
+          return dp[n-1][sum];
     }
 }
