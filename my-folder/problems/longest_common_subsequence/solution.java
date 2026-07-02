af9a794b8ad31d0da2eef1db0914951e69e48983
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-02 05:42:40 +0000

    Sync LeetCode submission - Longest Common Subsequence (java)

--- my-folder/problems/longest_common_subsequence/solution.java
+++ my-folder/problems/longest_common_subsequence/solution.java
@@ -2,17 +2,16 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m=text1.length();
         int n=text2.length();
-        int[] prev=new int[n+1];
+        int[][] dp=new int[m+1][n+1];
+        
         for(int i=1;i<=m;i++){
-        int[] curr=new int[n+1];
-        for(int j=0;j<=n;j++){
-            if(j==0)curr[j]=0;
-            else if(text1.charAt(i-1)==text2.charAt(j-1))curr[j]=1+prev[j-1];
-            else curr[j]=Math.max(curr[j-1],prev[j]);
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
         }
-        prev=curr;
-        }
-        return prev[n];
+        return dp[m][n];
+        
 
     }
 }
