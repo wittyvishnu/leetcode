dcff0c043c0bd115181d584942aed440381c0a1c
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-15 04:45:25 +0000

    Sync LeetCode submission - House Robber II (java)

--- my-folder/problems/house_robber_ii/solution.java
+++ my-folder/problems/house_robber_ii/solution.java
@@ -6,17 +6,17 @@ class Solution {
         return Math.max(construct(nums,0,n-2),construct(nums,1,n-1));
     }
     public int construct(int[] nums,int start,int end){
-        int n=nums.length;
+        int n=end-start+2;
         if(start==end)return nums[start];
-        int[] dp=new int[end-start+2];
+        int[] dp=new int[n];
         dp[0]=0;
         dp[1]=nums[start];
-        int idx=1;
+        int idx=2;
         for(int i=start+1;i<=end;i++){
+        dp[idx]=Math.max(dp[idx-1],dp[idx-2]+nums[i]);
         idx++;
-        dp[idx]=Math.max(dp[idx-2]+nums[i],dp[idx-1]);
         }
-        return dp[idx];
+        return dp[idx-1];
     }
 
 }
