3a58f26214a8499f4ac9fe9b6e29404e3b192f36
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-15 06:02:55 +0000

    Sync LeetCode submission - Edit Distance (java)

--- my-folder/problems/edit_distance/solution.java
+++ my-folder/problems/edit_distance/solution.java
@@ -2,18 +2,15 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m=word1.length();
         int n=word2.length();
-        int[] prev=new int[n+1];
-        for(int i=0;i<n+1;i++)
-        prev[i]=i;
-        for(int i=1;i<=m;i++){
-        int[] curr=new int[n+1];
-        curr[0]=i;
-        for(int j=1;j<=n;j++){
-            if(word1.charAt(i-1)==word2.charAt(j-1))curr[j]=prev[j-1];
-            else curr[j]=1+Math.min(prev[j-1],Math.min(curr[j-1],prev[j]));
-        }
-        prev=curr;
-        }
-        return prev[n];
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<=m;i++)
+        dp[i][0]=i;
+        for(int i=0;i<=n;i++)
+        dp[0][i]=i;
+        for(int i=1;i<=m;i++)
+        for(int j=1;j<=n;j++)
+        if(word1.charAt(i-1)==word2.charAt(j-1))dp[i][j]=dp[i-1][j-1];
+        else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+        return dp[m][n];
     }
 }
