c55934b30c191feebd66eb871f2822e7b0d39814
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-15 03:29:53 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (java)

--- my-folder/problems/pacific_atlantic_water_flow/solution.java
+++ my-folder/problems/pacific_atlantic_water_flow/solution.java
@@ -1,35 +1,28 @@
 class Solution {
-    class Pair{
-        int r;
-        int c;
-        Pair(int r,int c){
-            this.r=r;
-            this.c=c;
-        }
-    }
+    
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int m=heights.length;
         int n=heights[0].length;
         int[][] pacific=new int[m][n];
         int[][] atlantic=new int[m][n];
-        Queue<Pair> q=new ArrayDeque<>();
-        //pacific
+        Queue<int[]> q=new ArrayDeque<>();
+       
         for(int j=0;j<n;j++){
-            q.offer(new Pair(0,j));
+            q.offer(new int[]{0,j});
             pacific[0][j]=1;
         }
         for(int i=0;i<m;i++){
-            q.offer(new Pair(i,0));
+            q.offer(new int[]{i,0});
             pacific[i][0]=1;
         }
         bfs(heights,q,pacific,m,n);
-         //pacific
+        
         for(int j=0;j<n;j++){
-            q.offer(new Pair(m-1,j));
+            q.offer(new int[]{m-1,j});
             atlantic[m-1][j]=1;
         }
         for(int i=0;i<m;i++){
-            q.offer(new Pair(i,n-1));
+            q.offer(new int[]{i,n-1});
             atlantic[i][n-1]=1;
         }
         bfs(heights,q,atlantic,m,n);
@@ -42,21 +35,19 @@ class Solution {
 
 
     }
-    public void bfs(int[][] heights,Queue<Pair> q,int[][] arr,int m,int n){
+    public void bfs(int[][] heights,Queue<int[]> q,int[][] arr,int m,int n){
         int[] dx={-1,0,0,1};
         int[] dy={0,-1,1,0};
         while(!q.isEmpty()){
-            Pair temp=q.poll();
+            int[] node=q.poll();
             for(int d=0;d<4;d++){
-                int row=temp.r+dx[d];
-                int col=temp.c+dy[d];
-                if(row>=0&&row<m&&col>=0&&col<n&&arr[row][col] == 0&&heights[temp.r][temp.c]<=heights[row][col]){
-                    q.offer(new Pair(row,col));
-                    arr[row][col]=1;
+                int x=node[0]+dx[d];
+                int y=node[1]+dy[d];
+                if(x>=0 && y>=0 && x<m && y<n && arr[x][y]==0&&heights[x][y]>=heights[node[0]][node[1]]){
+                    q.offer(new int[]{x,y});
+                    arr[x][y]=1;
                 }
-
             }
-
         }
     }
 }
