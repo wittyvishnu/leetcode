cb4001541d7a5686b435adf1be91f919508d3097
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-25 04:40:32 +0000

    Sync LeetCode submission - Validate Binary Search Tree (java)

--- my-folder/problems/validate_binary_search_tree/solution.java
+++ my-folder/problems/validate_binary_search_tree/solution.java
@@ -1,12 +1,12 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     long val;
+ *     int val;
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
- *     TreeNode(long val) { this.val = val; }
- *     TreeNode(long val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -15,12 +15,14 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isvalid(root,Long.MIN_VALUE,Long.MAX_VALUE);
-        
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    public boolean isvalid(TreeNode root,long min,long max){
+    public boolean valid(TreeNode root,long min,long max){
         if(root==null)return true;
         if(root.val<=min || root.val>=max)return false;
-        return isvalid(root.left,min,root.val)&&isvalid(root.right,root.val,max);
+        boolean left=valid(root.left,min,root.val);
+        boolean right=valid(root.right,root.val,max);
+        if(left && right)return true;
+        return false;
     }
 }
