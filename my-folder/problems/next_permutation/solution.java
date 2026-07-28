7315a498d354685b6b7d47c9e5c23f54924c5518
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-28 03:20:28 +0000

    Sync LeetCode submission - Next Permutation (java)

--- my-folder/problems/next_permutation/solution.java
+++ my-folder/problems/next_permutation/solution.java
@@ -1,39 +1,36 @@
-import java.util.Arrays;
-
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int i = n - 2;
-
-        // Step 1: find first decreasing element
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
+        int n=nums.length;
+        int index=-1;
+        for(int j=n-2;j>=0;j--)
+        if(nums[j]<nums[j+1]){
+            index=j;
+            break;
         }
-
-        // Step 2: if pivot found
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums, i, j);
+        if(index==-1){
+            reverse(nums,0,n-1);
+            return;
         }
+        int pivot=-1;
+         for(int j=n-1;j>index;j--)
+         if(nums[index]<nums[j]){
+            pivot=j;
+            break;
+         }
+         int temp=nums[index];
+         nums[index]=nums[pivot];
+         nums[pivot]=temp;
+         reverse(nums,index+1,n-1);
 
-        // Step 3: reverse the suffix
-        reverse(nums, i + 1, n - 1);
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+    public void reverse(int[] nums,int l,int r){
+        while(l<r){
+            int temp=nums[r];
+            nums[r]=nums[l];
+            nums[l]=temp;
+            l++;
+            r--;
         }
+        
     }
 }
