eb85599989c8f82f8e18684303eae846fd21e786
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-07-29 10:30:49 +0000

    Sync LeetCode submission - Word Break (java)

--- my-folder/problems/word_break/solution.java
+++ my-folder/problems/word_break/solution.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet<String> set=new HashSet<>();
-        int maxLen=0;
-        for(String word:wordDict){
-            maxLen=Math.max(maxLen,word.length());
-            set.add(word);
+        int maxLen=Integer.MIN_VALUE;
+        for(String t:wordDict){
+        set.add(t);
+        maxLen=Math.max(maxLen,t.length());
         }
         int n=s.length();
         boolean[] dp=new boolean[n+1];
         dp[0]=true;
         for(int i=1;i<=n;i++){
             for(int j=i-1;j>=Math.max(0,i-maxLen);j--)
-            if(set.contains(s.substring(j,i))&&dp[j]){
-                dp[i]=true;
-                break;
+            if(dp[j]&&set.contains(s.substring(j,i))){
+            dp[i]=true;
+            break;
             }
-            
         }
         return dp[n];
     }
