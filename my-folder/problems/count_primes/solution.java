3e9b338087bb5ffd691408288b43390767d8aa7e
M Vishnu <147264691+wittyvishnu@users.noreply.github.com>
2026-08-01 05:37:15 +0000

    Sync LeetCode submission - Count Primes (java)

--- my-folder/problems/count_primes/solution.java
+++ my-folder/problems/count_primes/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int countPrimes(int n) {
-        int count=0;
-        int[] prime=new int[n];
+        boolean[] prime=new boolean[n];
         if(n<2)return 0;
-        prime[0]=1;
-        prime[1]=1;
-        
+        Arrays.fill(prime,true);
         for(int i=2;i*i<n;i++){
-            if(prime[i]==0)
-            for(int j=i*i;j<n;j+=i)
-            prime[j]=1;
-
+            if(prime[i]){
+                for(int j=i*i;j<n;j+=i)
+                prime[j]=false;
+            }
         }
+        int count=0;
         for(int i=2;i<n;i++)
-        if(prime[i]==0)count++;
+        if(prime[i])count++;
         return count;
     }
 }
